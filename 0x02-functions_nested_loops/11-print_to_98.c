e3679f53c9bf69f9258ba26c16aa26f7957961b2
OdongoAdamson <macnyakako@gmail.com>
2023-03-21 10:59:50 -0700

    Print 98 Battery street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - PRINT NUMBERS TO 98
+ * @n: number to be printed
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+	printf("%d", n);
+	printf(",");
+	printf(" ");
+	n++;
+	}
+	printf('\n');
+	return (0);
+}
