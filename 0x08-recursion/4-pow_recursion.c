e4f1317845a1a4885b114089d710f065c1cf9be2
OdongoAdamson <macnyakako@gmail.com>
2023-06-12 05:10:40 -0700

    Its impossible to eradicate an idea

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns value of x raised to y
+ *
+ * @x: number whose power is returned
+ *
+ * @y: number used as the power
+ *
+ * Return: Value of x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return -1;
+	}
+	return _pow_recursion(x ** y);
+}
