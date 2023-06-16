45757cc8a4a9a7f1b9613bae4d704696ae521ed0
OdongoAdamson <macnyakako@gmail.com>
2023-06-16 05:35:52 -0700

    Creating static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - fuctions that checks for uppercase
+ * @ i: character to be checked
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+
+	return 0;
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+# Static libraries tasks
+Task 0: create a static library
+Task 1: Create a script that creates a static library
