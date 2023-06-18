0a8c4099c6537391f17a3023f924e51241b92711
OdongoAdamson <macnyakako@gmail.com>
2023-06-18 05:28:24 -0700

    Print my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,11 @@
  * main - program that prints its name
  * @argc: argument counter
  * @argv: argument vector prints name
- * Return : Always 0 success
+ * Return: Always 0 success
  */
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
