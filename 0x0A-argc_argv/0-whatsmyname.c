c01f88b4c2598db8e07fe8e15a5000f1d97add85
OdongoAdamson <macnyakako@gmail.com>
2023-06-18 05:22:55 -0700

    Print my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * main - program that prints its name
+ * @argc: argument counter
+ * @argv: argument vector prints name
  * Return : Always 0 success
  */
 int main(int argc, char **argv)
 {
-	while (argc--)
-		printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
+	return (0);
 }
 
