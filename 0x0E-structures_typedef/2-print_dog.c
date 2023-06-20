e0803c024474ed72e45b457452439d1f6117472f
OdongoAdamson <macnyakako@gmail.com>
2023-06-20 04:23:44 -0700

    A dog will teach you unconditional love

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * print_dog -	prints details of dog
+ * @d: name of dog
+ * Return: Always 0 success
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
