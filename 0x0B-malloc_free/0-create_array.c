5f89ad85cfb155f7ad501f3baafcf985d3581b92
OdongoAdamson <macnyakako@gmail.com>
2023-06-20 08:15:18 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,14 +8,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *c[size];
+	unsigned int i;
+	char *str;
 
-	if (size != 0)
-	{
-		return (c);
-	}
-	else
-	{
+	if (size == 0)
 		return (NULL);
+	str = malloc(sizeof(char) * size);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
 	}
+	return (str);
 }
