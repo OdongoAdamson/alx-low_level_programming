cf9f18f695315d39961abe82d878f2de59e01ffb
OdongoAdamson <macnyakako@gmail.com>
2023-06-21 06:56:24 -0700

    create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - finds ints
+ * @array: integers
+ * @size: size of array
+ * @cmp: compaires ints
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL & cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++
+			}
+		}
+	}
+	return (-1);
+}
