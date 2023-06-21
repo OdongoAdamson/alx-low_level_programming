ed65e37478ba339186c1ee4ca3a023afe5d67c52
OdongoAdamson <macnyakako@gmail.com>
2023-06-21 04:39:59 -0700

    Whats my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTERS_H
+#ifndef _HEADER_
+#define _HEADER_
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
