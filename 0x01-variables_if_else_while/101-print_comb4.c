a76d461b2d26af7c66f9e56215f795b22c15794e
OdongoAdamson <macnyakako@gmail.com>
2023-09-22 03:37:21 -0700

    Possible combs of 3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @ i - first digit
+ * @ j - second digit
+ * @ k - third digit
+ * return: Always zero success
+ */
+
+int main(){
+	int i, j, k;
+	for (i = 0; i <= 9; i++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	for (k = 0; k <= 9; k++)
+	{
+	putchar(i);
+	putchar(j);
+	putchar(k);
+	putchar(',');
+	}
+	}
+	}
+	return (0)
+}
