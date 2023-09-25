0d588264e695c645aad0093dc09ecceb44ed9c1e
OdongoAdamson <macnyakako@gmail.com>
2023-09-25 11:59:46 -0700

    Prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_number - prints nums 0 - 9
+ * Return: Always 0 (success)
+ */
+
+void print_number(void)
+{
+	for (i = 48; i <= 57; i++)
+	{
+	_putchar(i);
+	}
+	_putchar("\n");
+}
