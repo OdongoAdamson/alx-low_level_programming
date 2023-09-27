384958036375eeb66106d6ff72bd25e7c12712b4
OdongoAdamson <macnyakako@gmail.com>
2023-09-27 11:34:26 -0700

    Print string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-	count += 1;
+	return (0);
 	}
-	_strlen_recursion(s);
-	return (count);
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
