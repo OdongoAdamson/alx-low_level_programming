204b7ffaa2278fc7c4bbf20ad8d3bd8ce62fef3d
OdongoAdamson <macnyakako@gmail.com>
2023-10-03 05:29:56 -0700

    Creat array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,21 +12,21 @@ char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i;
-	
+
 	if (size == 0)
 	{
-		return (NULL);
+	return (NULL);
 	}
-	
+
 	s = malloc(sizeof(char) * size);
-	
+
 	if (s == NULL)
 	{
-		return(NULL);
+	return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
-		s[i] = c;
+	s[i] = c;
 	}
 
 	return (s);
