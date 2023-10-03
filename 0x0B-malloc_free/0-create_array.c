31120d03fd2c26e34dc3d0ba181e9f169341a45f
OdongoAdamson <macnyakako@gmail.com>
2023-10-03 00:18:04 -0700

    Added c file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_array - fucntions that creates an array of char
+ * @c : characters
+ * @size : size of mem allocated
+ * Return: NULL if size == 0 else pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
