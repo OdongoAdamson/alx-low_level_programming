a5fd55938169caf6df2db0070fbe390c13247a64
OdongoAdamson <macnyakako@gmail.com>
2023-10-04 12:14:02 -0700

    Adds intergers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - adds integer numbers
+ * @argc : count of arguments
+ * @argv : strings passed to amin
+ * Return: Always 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += (atoi(argv[i]);
+				if (isaplha(argv[i])
+					{
+					printf("Error\n");
+					return (i);
+					}
+					else if (argc < 2)
+					{
+					putchar(48);
+					putchar("\n");
+					}
+					else
+					{
+					printf("%d\n", sum);
+					}
+					}
+					}
+
