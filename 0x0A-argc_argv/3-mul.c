6c5babd683a4ded5cf03d6ba46925feaf0f7ee8f
OdongoAdamson <macnyakako@gmail.com>
2023-10-04 07:52:50 -0700

    Prints result of multiplictaionof two

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,9 +14,17 @@ int main(int argc, char *argv[])
 	(void) argc;
 	int mul;
 
-	mul = argv[1] * argv[2];
+	mul = (argv[1] * argv[2]);
 
+	if (argc < 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
 	printf("%d\n", mul);
+	}
 
 	return (0);
 }
