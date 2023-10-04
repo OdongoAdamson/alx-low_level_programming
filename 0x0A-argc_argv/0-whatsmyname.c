92c1bb9214f21535bf46be89adda65441f8ae9d1
OdongoAdamson <macnyakako@gmail.com>
2023-10-04 06:18:32 -0700

    Program says its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - returns a program name
+ * @argc : number of arguments
+ * @argv : strin(s) of arguments passed
+ * Return: Always 0 Success
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%s", argv[0]);
+	printf('\n');
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,7 @@
+#Arguments to main tasks
+Task 0 - Prints its own name
+Task 1 - Prints number of arguments passed to it
+Task 2 - Prints all the arguments
+Task 3 - Miltiplies two numbers
+Task 4 - adds positive numbers
+
