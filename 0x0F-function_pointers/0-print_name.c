7deaeb4a070dd16e3603c42043dbea2fad21d5a8
OdongoAdamson <macnyakako@gmail.com>
2023-10-11 11:50:21 -0700

    Prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @f: function to name
+ * @name: name of person
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+	{
+		f(name);
+	}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,6 @@
+#function pointers
+Task 0 - print name
+Task 1 - execute a function
+Task 2 - searches for integers
+Task 3 - performs simple operations
+
