7d44d904d16d92b0881669aba970408ad7da2432
OdongoAdamson <macnyakako@gmail.com>
2023-10-11 12:20:09 -0700

    Searches ints

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointer.h"
+
+/**
+ * int_index - searches index
+ * @array: elements being checked
+ * @size: number of elements
+ * @cmp: function being used
+ *
+ * Return: Always 0 success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array !- NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
+	}
+	return (-1);
+}
