0588afe1d36041312c93ce8c97c2d88d98ad81ab
OdongoAdamson <macnyakako@gmail.com>
2023-10-12 13:09:32 -0700

    Prints strings

---
+++ 2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @n: number of strings
+ * @separator: separates strings
+ * Return : strings literals
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list strngs;
+char *s;
+unsigned int i;
+
+va_start(strngs, n);
+
+for (i = 0; i < n; i++)
+{
+s = va_arg(strngs, char *);
+
+if (s == NULL)
+printf("nil");
+else
+printf("%s", str);
+
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+
+printf("\n");
+
+va_end(strngs);
+}
