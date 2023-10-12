9790598b1124df886e68ba623e543902bd1816ff
OdongoAdamson <macnyakako@gmail.com>
2023-10-12 11:55:32 -0700

    Sums them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - prints sum of all
+ * @n : numbers to be summed
+ *
+ * Return: Sum of numbers
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	va_start(ap, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,6 @@
+#Varriadic functions
+Task 0 - Sum of all parameters
+Task 1 - Print numbers
+Task 2 - Prints strings
+Task 3 - Prints anything
+
