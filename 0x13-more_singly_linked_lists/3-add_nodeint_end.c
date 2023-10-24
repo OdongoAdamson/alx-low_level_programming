af855f642c29d6d31095ecc74199d4d0c6b395e3
OdongoAdamson <macnyakako@gmail.com>
2023-10-24 09:12:59 -0700

    insert at the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add node ata the end
+ * @head: pointer to start of list
+ * @n: data to be inserted
+ *
+ * Return: pointer to ned element
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_end;
+	listint_t *tmp = *head;
+
+	new_end = malloc(sizeof(listint_t));
+
+	if (!new_end)
+		return (NULL);
+
+	new_end->n = n;
+	new_end->next = NULL;
+
+	if(*head == NULL)
+	{
+		*head = new_end;
+		return (new_end);
+	}
+
+	while (tmp->next)
+	tmp = tmp->next;
+
+	tmp->next = new_end;
+
+	return (new_end);
+}
