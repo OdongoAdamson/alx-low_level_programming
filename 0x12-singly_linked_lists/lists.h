61ff3d1e61edc2ef42ad358ae96bb90514a584dd
OdongoAdamson <macnyakako@gmail.com>
2023-10-24 04:21:27 -0700

    header file changes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * struct list_s - sugly linked list
+ * @str: string 
+ * @len: length of the string
+ * @next: point of the node
+ *
+ * Decription: singly linked list
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
