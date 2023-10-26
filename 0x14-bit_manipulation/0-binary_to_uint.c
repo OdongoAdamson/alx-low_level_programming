76698079254ea8d51a1d6a2df9e6e175709bcaa7
OdongoAdamson <macnyakako@gmail.com>
2023-10-26 14:00:29 -0700

    Return unsigned in value

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to an int
+ * @b: sting being converted
+ *
+ * Return: 0 if char or null 1 is okay
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int slen = strlen(b);
+	int total = 0;
+	int decval = 1;
+
+	for (int i = (slen - 1); i >= 0; i--)
+	{
+		if ((b[i] != '0' && b[i] != '1') | b[i] == NULL)
+			return (0)
+		if (b[i] == '1')
+			total += decval;
+		decval *= 2;
+	}
+
+
+	return (total);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,8 @@
+#Bit manipulation tasks
+Task 0 - convert binary to int
+Task 1 - Print binary rep of a number
+Task 2 - Return value of a bit at an index
+Task 3 - Set the value of a bit to 1
+Task 4 - Set value to 0
+Task 5 - Returns number of bits to be flipped
+
