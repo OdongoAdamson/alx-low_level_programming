862ad8c3bd03eb353fdf0134baac34cbee6895e6
OdongoAdamson <macnyakako@gmail.com>
2023-10-30 14:39:31 -0700

    reads a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - read text file
+ * @filename: the text file
+ * @letter: letters being read
+ * Return: w- actual number and 0 if it fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *bf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bf = malloc(sizeof(char) * letters);
+	t = read(fd, bf, letters);
+	w = write(STDOUT_FILENO, bf, t);
+
+	free(bf);
+	close(fd);
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,6 @@
+# 0x15-file IO Tasks
+Task 0 - Read text and prints it to POsix
+Task 1 - Create a file
+Task 2 - Append text at end of file
+Task 3 - Cpoy content
+
