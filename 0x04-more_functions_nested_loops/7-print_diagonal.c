821b3ef623c3334daffe70ab43b3b8e6880def24
OdongoAdamson <macnyakako@gmail.com>
2023-11-10 05:20:13 -0800

    prints diagnonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@
 
 void print_diagonal(int n)
 {
-	int x;
+	int x, y;
 
 	if (n <= 0)
 	{
@@ -17,8 +17,12 @@ void print_diagonal(int n)
 	{
 	for (x = 0; x < n; x++)
 	{
-	_putchar(92);
+	for (y = 0; y < x; y++)
+	{
+	_putchar(32);
 	}
+	_putchar(92);
 	_putchar('\n');
+	}
 	}
 }
