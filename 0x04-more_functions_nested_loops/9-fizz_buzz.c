ce3498d46f2d1e57cc2bb9adc87f92a6ddda714f
OdongoAdamson <macnyakako@gmail.com>
2023-11-12 01:02:01 -0800

    Fizz Buzz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,29 +10,29 @@ int main(void)
 
 	int x;
 
-	for (x = 1; x < 101; x++)
+	for (x = 1; x <= 100; x++)
 	{
-	if (x % 15 == 0)
+	if ((x % 3 == 0) && (x % 5 == 0))
 	{
 	printf("FizzBuzz");
-	printf(" ");
 	}
 	else if (x % 3 == 0)
 	{
 	printf("Fizz");
-	printf(" ");
 	}
 	else if (x % 5 == 0)
 	{
 	printf("Buzz");
-	printf(" ");
 	}
 	else
 	{
 	printf("%d", x);
+	}
+	if (x != 100)
+	{
 	printf(" ");
 	}
 	}
-	printf('\n');
+	printf("\n");
 	return (0);
 }
