48dd5ab298e217ae389c058b484d41f0e77a89d4
OdongoAdamson <macnyakako@gmail.com>
2023-11-24 22:58:05 -0800

    Binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int slen = strlen(b);
-	int total = 0;
-	int decval = 1;
+	int i;
+	unsigned int decval;
+	
+	decval = 0;
 
-	for (int i = (slen - 1); i >= 0; i--)
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0' && b[i] != '1') | b[i] == NULL)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		if (b[i] == '1')
-			total += decval;
-		decval *= 2;
 	}
-
-
-	return (total);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		decval <<= 1;
+		if (b[i] == '1')
+			decval += 1;
+	}
+	return (decval);
 }
